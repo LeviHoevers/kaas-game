c9050c44e7f59c56c6f95799c9e7b23983a7b896
Levi Hoevers <99065078@mydavinci.nl>
2020-09-27 12:04:48 +0200

    update

--- kaas-game-master (1)/game opdracht/game.js
+++ kaas-game-master (1)/game opdracht/game.js
@@ -12,45 +12,42 @@ var name = prompt("OAK: I study Pokemon as profession well, enough about me.\n t
 alert ("OAK: Nice to meet you" + " " + name + " " + "now your adventure begins in the world of pokemon!")
 var firstmove = prompt("you're adventure begins on the doorstep of your new home, you're heading towards the Pokemon lab.\n you have 4 options to go to the left,right,up or down");
 firstmove = firstmove.toLowerCase();
-	if  (firstmove == "down") {  alert("u made it safely to prof. oak's lab")
-}
-	else if (firstmove == "up")
-	alert("u headed upwards. u walked through some tall grass but have no pokemon to defend yourself! game over")
-}
-	else if (firstmove == "right") alert("hej hej")
-		
-
-	
-alert ("OAK: Start your new adventure by chosing your starter Pokemon.\n");
- var starter = prompt("Choose ur starter Pokemon!\n  - Bulbasaur \n  - Squirtle \n  - Charmander");
- starter = starter.toUpperCase();
- 
-		if (starter == "Bulbasaur") {
-		alert("you chose the grass-type Pokemon: Bulbasaur!")
-		alert ("u received the" + " " + starter + " " + "from PROF. OAK")
-		alert("*Gary your rival runs in*\n GARY:" + " " + name + " " + " now that we both have pokemon i challenge u for a Pokemon battle!\n Charmander I choose you!");
-		alert("Bulbasaur has 2 moves at LVL.5, Tackle and Growl. Tackle inflicts damage on your opponent and Growl lowers your opponents attack stats");
-		var movea = prompt ("your Bulbasaur has 20hp, choose your move\n - Tackle\n40pp \n type/normal  \n - Growl \n40pp \n type/normal");
-		movea = movea.toLowerCase();
-			if (movea == "growl") {
-			alert("Bulbasaur used Growl! \n Foe Charmander's attack fell!");
-			alert("Charmander used Scratch!\n your Bulbasaur's HP went down by 7 (13/20)");
-			var moveb = prompt("What will Bulbasaur do? ")
-			moveb = moveb.toLowerCase();
-				if (moveb == "tackle") {
-				alert("Bulbasaur used Tackle! Critikal Hit!!\n Charmander's HP went down by 13 (06/19)");
-				alert("Charmander used Scratch! \n Your Bulbasaur's HP went down by 4 (11/20) ");
-				
-				
+	if (firstmove == "down") {
+	alert("u made it safely to prof. oak's lab")
+				alert ("OAK: Start your new adventure by chosing your starter Pokemon.\n");
+ 				var starter = prompt("Choose ur starter Pokemon!\n  - Bulbasaur \n  - Squirtle \n  - Charmander");
+ 				starter = starter.toUpperCase();
+					if (starter == "bulbasaur") {
+					alert("you chose the grass-type Pokemon: Bulbasaur!")
+					alert ("u received the" + " " + starter + " " + "from PROF. OAK")
+					alert("*Gary your rival runs in*\n GARY:" + " " + name + " " + " now that we both have pokemon i challenge u for a Pokemon battle!\n Charmander I choose you!");
+					alert("Bulbasaur has 2 moves at LVL.5, Tackle and Growl. Tackle inflicts damage on your opponent and Growl lowers your opponents attack stats");
+					var movea = prompt ("your Bulbasaur has 20hp, choose your move\n - Tackle\n40pp \n type/normal  \n - Growl \n40pp \n type/normal");
+					movea = movea.toLowerCase();
+						if (movea == "growl") {
+						alert("Bulbasaur used Growl! \n Foe Charmander's attack fell!");
+						alert("Charmander used Scratch!\n your Bulbasaur's HP went down by 7 (13/20)");
+						var moveb = prompt("What will Bulbasaur do? ")
+						moveb = moveb.toLowerCase();
+							if (moveb == "tackle") {
+						alert("Bulbasaur used Tackle! Critikal Hit!!\n Charmander's HP went down by 13 (06/19)");
+						alert("Charmander used Scratch! \n Your Bulbasaur's HP went down by 4 (11/20) ");
+					}
+					else if (starter == "SQUIRTLE") 
+					alert("you chose the water-type Pokemon: Squirtle!");
+					}
+					else if (starter == "CHARMANDER") {
+					alert("you chose the fire-type Pokemon: Charmander!")
+					}
+	else if (firstmove == "up") {
+	alert("u headed upwards. u walked through some tall grass and encountered a wild Pokemon. but u have no pokemon to defend yourself! game over")
+	}
+	else if (firstmove == "right") {
+	alert("hej hej")
 			}
 		}
+	}
 }
-	else if (starter == "SQUIRTLE") {
-		alert("you chose the water-type Pokemon: Squirtle!");
-}
-	else if (starter == "CHARMANDER") {
-		alert("you chose the fire-type Pokemon: Charmander!");
-}
-	
-alert("sorry but you're not old enough to play this game.\n ");
+alert ("you're too young to play this game"){
 
+}
